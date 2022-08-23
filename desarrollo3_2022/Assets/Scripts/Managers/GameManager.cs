b9dcfd83b4d2f8a762d9fa5bda1c82d09983b7a7
Guido Tello <lpmaleinn@gmail.com>
2022-08-23 12:29:25 -0300

    Added in-game pause panel

---
+++ desarrollo3_2022/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+
+    public class GameManager : MonoBehaviour
+    {
+        #region VARIABLES
+        #region SERIALIZED VARIABLES
+
+        #endregion
+
+        #region STATIC VARIABLES
+        public static bool GameRunning { get; private set; } = true;
+        #endregion
+
+        #region PRIVATE VARIABLES
+
+        #endregion
+        #endregion
+
+        #region METHODS
+        #region PUBLIC METHODS
+
+        #endregion
+
+        #region STATIC METHODS
+
+        #endregion
+
+        #region PRIVATE METHODS
+
+        #endregion
+        #endregion
+    }
+
---
+++ desarrollo3_2022/Assets/Scripts/Pause/PauseSystem.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using System;
+
+
+    public class PauseSystem : MonoBehaviour
+    {
+        public static Action<PauseStates> OnPauseStateChange;
+        public static bool Paused { get; private set; }
+        private void Awake()
+        {
+            InputManager.OnPausePress += PauseControl;
+        }
+        private void OnDestroy()
+        {
+            InputManager.OnPausePress -= PauseControl;
+        }
+
+        private void Start()
+        {
+            Paused = false;
+            PauseControl();
+        }
+
+        public static void PauseControl()
+        {
+            if (GameManager.GameRunning)
+            {
+                Paused = !Paused;
+                Time.timeScale = Paused ? 0 : 1;
+                PauseStates actualState = Paused ? PauseStates.Paused : PauseStates.Resumed;
+                Debug.Log("Envio estado de pausa: " + Paused);
+                OnPauseStateChange?.Invoke(actualState);
+            }
+        }
+        
+    }
