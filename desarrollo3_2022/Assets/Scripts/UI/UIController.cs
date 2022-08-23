37da6dd86688b8825595db0c370191f3f8344dd0
Guido Tello <lpmaleinn@gmail.com>
2022-08-23 12:40:04 -0300

    Updated pause panel

--- desarrollo3_2022/Assets/Scripts/UI/UIController.cs
+++ desarrollo3_2022/Assets/Scripts/UI/UIController.cs
@@ -3,67 +3,70 @@ using UnityEngine;
 using TMPro;
 
 
-    public class UIController : MonoBehaviour
+public class UIController : MonoBehaviour
+{
+    #region VARIABLES
+    #region SERIALIZED VARIABLES
+    [SerializeField] private CanvasGroup pausePanel;
+
+    [Header("Menu scene")]
+    [SerializeField] private string menuSceneName = "";
+    #endregion
+
+    #region STATIC VARIABLES
+
+    #endregion
+
+    #region PRIVATE VARIABLES
+    #endregion
+    #endregion
+
+    #region METHODS
+    #region PUBLIC METHODS
+
+    #endregion
+
+    #region STATIC METHODS
+
+    #endregion
+
+    #region PRIVATE METHODS
+    private void Awake()
     {
-        #region VARIABLES
-        #region SERIALIZED VARIABLES
-        [SerializeField] private CanvasGroup pausePanel;
-        #endregion
-
-        #region STATIC VARIABLES
-
-        #endregion
-
-        #region PRIVATE VARIABLES
-        #endregion
-        #endregion
-
-        #region METHODS
-        #region PUBLIC METHODS
-
-        #endregion
-
-        #region STATIC METHODS
-
-        #endregion
-
-        #region PRIVATE METHODS
-        private void Awake()
-        {
-            PauseSystem.OnPauseStateChange += PausePanelController;
-        }
-        private void OnDestroy()
-        {
-            PauseSystem.OnPauseStateChange -= PausePanelController;
-        }
-        private void PausePanelController(PauseStates state)
-        {
-            if(state == PauseStates.Resumed)
-            {
-                HidePanel(pausePanel); //if it is resumed, then, i should show the message
-            }
-            else
-            {
-                ShowPanel(pausePanel);
-            }
-        }
-        private void ShowPanel(CanvasGroup panel)
-        {
-            panel.alpha = 1;
-            panel.blocksRaycasts = true;
-            panel.interactable = true;
-        }
-        private void HidePanel(CanvasGroup panel)
-        {
-            panel.blocksRaycasts = false;
-            panel.interactable = false;
-            panel.alpha = 0;
-        }
-        public void BackToMenu()
-        {
-            SceneManager.LoadScene("Menu");
-        }
-        #endregion
-        #endregion
+        PauseSystem.OnPauseStateChange += PausePanelController;
     }
+    private void OnDestroy()
+    {
+        PauseSystem.OnPauseStateChange -= PausePanelController;
+    }
+    private void PausePanelController(PauseStates state)
+    {
+        if (state == PauseStates.Resumed)
+        {
+            HidePanel(pausePanel); //if it is resumed, then, i should show the message
+        }
+        else
+        {
+            ShowPanel(pausePanel);
+        }
+    }
+    private void ShowPanel(CanvasGroup panel)
+    {
+        panel.alpha = 1;
+        panel.blocksRaycasts = true;
+        panel.interactable = true;
+    }
+    private void HidePanel(CanvasGroup panel)
+    {
+        panel.blocksRaycasts = false;
+        panel.interactable = false;
+        panel.alpha = 0;
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(menuSceneName);
+    }
+    #endregion
+    #endregion
+}
 
